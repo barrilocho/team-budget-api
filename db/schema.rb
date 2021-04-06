6db494ab98306283b83267170ccd16bfd04c212f
Alejandro Barrios <alejandro@aleate.com>
2021-04-05 21:41:16 -0500

    feat(user): add users model and relations with family

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_05_021830) do
+ActiveRecord::Schema.define(version: 2021_04_06_023940) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,4 +23,16 @@ ActiveRecord::Schema.define(version: 2021_04_05_021830) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "auth_token"
+    t.bigint "family_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["family_id"], name: "index_users_on_family_id"
+  end
+
+  add_foreign_key "users", "families"
 end
