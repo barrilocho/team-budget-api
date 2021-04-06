0bdb8e4b9ab611c9d683a2c19722c43ef5829017
Alejandro Barrios <alejandro@aleate.com>
2021-04-06 00:37:31 -0500

    feat(category-validations): add category validations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,19 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id         :bigint           not null, primary key
+#  name       :string
+#  family_id  :bigint           not null
+#  type       :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Category < ApplicationRecord
   belongs_to :family
   has_many :movements
+  
+  validates :name, presence:true
+  validates :family_id, presence:true
+  validates :type, presence:true
 end
