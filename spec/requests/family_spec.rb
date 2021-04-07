6240e339229c08bdbe979c4b088dc5b35ee6dca7
Alejandro Barrios <alejandro@aleate.com>
2021-04-07 00:16:45 -0500

    feat(test-family): add test for update and create family

--- spec/requests/family_spec.rb
+++ spec/requests/family_spec.rb
@@ -32,4 +32,59 @@ RSpec.describe 'Families', type: :request do
       expect(response).to have_http_status(200)
     end
   end
+  describe "POST /families" do
+    it "should create a family" do 
+      req_payload = {
+        alias_name: "Barrios",
+        code: "12345",
+        members: 1,
+        balance: 0
+      } 
+      post "/families", params: req_payload
+      payload = JSON.parse(response.body)
+      expect(payload).to_not be_empty
+      expect(payload['id']).to_not be_empty
+      expect(response).to have_http_status(:created)
+    end
+    it "should return error message on invalid family" do 
+      req_payload = {
+        alias_name: "Barrios",
+        members: 1,
+        balance: 0
+      } 
+      post "/families", params: req_payload
+      payload = JSON.parse(response.body)
+      expect(payload).to_not be_empty
+      expect(payload['error']).to_not be_empty
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+  describe "PUT /families/#{id}" do
+    let!(:family) {create(:family)}
+    it "should update a family" do  
+      req_payload = {
+        alias_name: "Barrios test",
+        code: "12345678",
+        members: 1,
+        balance: 0
+      } 
+      post "/families/#{family.id}", params: req_payload
+      payload = JSON.parse(response.body)
+      expect(payload).to_not be_empty
+      expect(payload['id']).to eq(family.id)
+      expect(response).to have_http_status(:ok)
+    end
+    it "should return error message on invalid family update" do 
+      req_payload = {
+        alias_name: nil,
+        members: nil,
+        balance: 0
+      } 
+      post "/families/#{family.id}", params: req_payload
+      payload = JSON.parse(response.body)
+      expect(payload).to_not be_empty
+      expect(payload['error']).to_not be_empty
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
 end
