abf79053367bcfb8c6550705aa8edcec4874c35f
Alejandro Barrios <alejandro@aleate.com>
2021-04-07 00:43:28 -0500

    feat(update-create-family): Add routes for and methods for create and update families

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -1,4 +1,14 @@
 class FamiliesController < ApplicationController
+
+  rescue_from Exception do |e|
+    render json: {error: e.message}, status: :internal_error
+  end
+
+  rescue_from 'ActiveRecord::RecordInvalid' do |e|
+    render json: {error: e.message}, status: :unprocessable_entity
+  end
+
+
   def index
     @families = Family.all
     render json: @families, status: :ok
@@ -8,5 +18,28 @@ class FamiliesController < ApplicationController
   def show
     @family = Family.find(params[:id])
     render json: @family, status: :ok
+  end
+
+  #POST /families
+  def create
+    @family = Family.create!(create_params)
+    render json: @family, status: :created
+  end
+
+  #PUT /families/{id}
+  def update
+    @family = Family.find(params[:id])
+    @family.update!(update_params)
+    render json: @family, status: :ok
+  end
+
+  private
+
+  def create_params
+    params.required(:family).permit(:alias_name, :balance, :code, :members)
+  end
+
+  def update_params
+    params.required(:family).permit(:alias_name)
   end
 end
--- spec/requests/family_spec.rb
+++ spec/requests/family_spec.rb
@@ -35,22 +35,26 @@ RSpec.describe 'Families', type: :request do
   describe "POST /families" do
     it "should create a family" do 
       req_payload = {
-        alias_name: "Barrios",
-        code: "12345",
-        members: 1,
-        balance: 0
+        family: {
+          alias_name: "Barrios",
+          code: "12345",
+          members: 1,
+          balance: 0
+        }
       } 
       post "/families", params: req_payload
       payload = JSON.parse(response.body)
       expect(payload).to_not be_empty
-      expect(payload['id']).to_not be_empty
+      expect(payload['id']).to_not be_nil
       expect(response).to have_http_status(:created)
     end
     it "should return error message on invalid family" do 
       req_payload = {
-        alias_name: "Barrios",
-        members: 1,
-        balance: 0
+        family:{
+          alias_name: "Barrios",
+          members: 1,
+          balance: 0
+        }
       } 
       post "/families", params: req_payload
       payload = JSON.parse(response.body)
@@ -59,16 +63,18 @@ RSpec.describe 'Families', type: :request do
       expect(response).to have_http_status(:unprocessable_entity)
     end
   end
-  describe "PUT /families/#{id}" do
+  describe "PUT /families/id" do
     let!(:family) {create(:family)}
     it "should update a family" do  
       req_payload = {
-        alias_name: "Barrios test",
-        code: "12345678",
-        members: 1,
-        balance: 0
+        family:{
+          alias_name: "Barrios test",
+          code: "12345678",
+          members: 1,
+          balance: 0
+        }
       } 
-      post "/families/#{family.id}", params: req_payload
+      put "/families/#{family.id}", params: req_payload
       payload = JSON.parse(response.body)
       expect(payload).to_not be_empty
       expect(payload['id']).to eq(family.id)
@@ -76,11 +82,13 @@ RSpec.describe 'Families', type: :request do
     end
     it "should return error message on invalid family update" do 
       req_payload = {
-        alias_name: nil,
-        members: nil,
-        balance: 0
+        family: {
+          alias_name: nil,
+          members: nil,
+          balance: 0
+        }
       } 
-      post "/families/#{family.id}", params: req_payload
+      put "/families/#{family.id}", params: req_payload
       payload = JSON.parse(response.body)
       expect(payload).to_not be_empty
       expect(payload['error']).to_not be_empty
