a5c36f81907de0048a6d47dd778659a6dab73741
Alejandro Barrios <alejandro@aleate.com>
2021-04-07 21:03:10 -0500

    feat(category-tests): Implement test for categories

---
+++ spec/requests/category_spec.rb
@@ -0,0 +1,94 @@
+require "rails_helper"
+require "byebug"
+
+RSpec.describe 'Categories', type: :request do
+
+  describe 'GET /categories' do
+    let!(:categories) { create_list(:category, 10) }
+    before { get '/categories' }
+    it 'should return OK' do 
+      payload = JSON.parse(response.body)
+      expect(payload).not_to be_empty
+      expect(response).to have_http_status(200)
+    end
+  end
+  describe "with data in DB" do
+    let!(:categories) { create_list(:category, 10) }
+    before { get '/categories' }
+    it "should return all the categories" do  
+      payload = JSON.parse(response.body)
+      expect(payload.size).to eq(categories.size)
+      expect(response).to have_http_status(200)
+    end
+  end
+  describe "GET /categories/{id}" do
+    let!(:category) { create(:category) }
+    it "should return a category" do  
+      get "/categories/#{category.id}"
+      payload = JSON.parse(response.body)
+      expect(payload).to_not be_empty
+      expect(payload['id']).to eq(category.id)
+      expect(response).to have_http_status(200)
+    end
+  end
+  describe "POST /categories" do
+    it "should create a category" do 
+      let!(:family) { create(:family) }
+      req_payload = {
+        category: {
+          name: "Entretenimiento",
+          type: 1,
+          family_id: family.id
+        }
+      } 
+      post "/categories", params: req_payload
+      payload = JSON.parse(response.body)
+      expect(payload).to_not be_empty
+      expect(payload['id']).to_not be_nil
+      expect(response).to have_http_status(:created)
+    end
+    it "should return error message on invalid category" do 
+      req_payload = {
+        category:{
+          name: "Entretenimiento",
+          type: 1
+        }
+      } 
+      post "/categories", params: req_payload
+      payload = JSON.parse(response.body)
+      expect(payload).to_not be_empty
+      expect(payload['error']).to_not be_empty
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+  describe "PUT /categories/id" do
+    let!(:category) {create(:category)}
+    it "should update a category" do  
+      req_payload = {
+        category:{
+          name: "Diversión",
+          type: 2
+        }
+      } 
+      put "/categories/#{category.id}", params: req_payload
+      payload = JSON.parse(response.body)
+      expect(payload).to_not be_empty
+      expect(payload['id']).to eq(category.id)
+      expect(response).to have_http_status(:ok)
+    end
+    it "should return error message on invalid category update" do 
+      req_payload = {
+        category: {
+          name: "Diversión",
+          type: 2,
+          family_id: nil
+        }
+      } 
+      put "/categories/#{category.id}", params: req_payload
+      payload = JSON.parse(response.body)
+      expect(payload).to_not be_empty
+      expect(payload['error']).to_not be_empty
+      expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+end
--- test/factories/categories.rb
+++ test/factories/categories.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :category do
     name { Faker::Internet.user_name }
-    type { 1 }
+    type { [1,2].sample }
     family
   end
 end
