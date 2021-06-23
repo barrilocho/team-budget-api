6c6722c2ba71cdcd394e57b7e5a9abb035fcd42f
barrilocho <barrilocho@gmail.com>
2021-06-22 22:52:58 -0500

    feat(family_user): add family user create for register method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,24 @@ class UsersController < ApplicationController
   # skip_before_action :authorized, only: [:create]
   before_action :authorized, only: []
 
+  # REGISTER NEW FAMILY
+  def createFamilyUser
+    @family = Family.new(family_params)
+    @family.code = 
+    @family.members = 1
+    @family.balance = 0
+    @family.save
+    @user = User.new(user_params)
+    @user.family_id = @family.id
+    @user.save
+    if @user.valid?
+      token = encode_token({user_id: @user.id})
+      render json: {user: @user, token: token} 
+    else
+      render json: {erros: @user.errors.full_messages}
+    end
+  end
+
   # REGISTER
   def create 
     @user = User.create(user_params)
@@ -21,6 +39,10 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.permit(:username, :password, :email, :first_name, :last_name)
+    params.require(:user).permit(:username, :password, :email, :first_name, :last_name)
+  end
+
+  def family_params
+    params.require(:family).permit(:alias_name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/health', to: 'health#health'
   resources :users
   post 'login', to: 'auth#login'
-
+  post 'createFamilyUser', to: 'users#createFamilyUser'
   resources :families, only: [:index, :show, :create, :update]
   resources :categories, only: [:index, :show, :create, :update]
 end
